d73ca4ef049227be68561fb78bb21156f04c80db
Boldbaatar Baldan-Ochir <boroog@gmail.com>
2023-02-18 21:28:51 +0800

    .

--- src/configs/hosts_conf.js
+++ src/configs/hosts_conf.js
@@ -1,7 +1,7 @@
 module.exports = {
     trusted_hosts: ['localhost', '192.168.0.*', '192.168.1.*'],
     allowed_cors: {
-        'qpay.mn': ['api.qpay.mn', 'merchant-sandbox.qpay.mn']
+        'domain.mn': ['api.domain.mn', 'merchant-sandbox.domain.mn']
     },
     proxy_ips: [''],
 }
